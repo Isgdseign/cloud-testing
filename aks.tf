e7105332a8ddcd2c1a5b1d885cb15e5140ed6cd7
Ishaq Ali <177396444+Isgdseign@users.noreply.github.com>
2026-07-20 08:48:38 +0500

    GRAVIA AI auto-fix applied for aks.tf

--- aks.tf
+++ aks.tf
@@ -3,6 +3,11 @@ resource azurerm_kubernetes_cluster "k8s_cluster" {
   location            = var.location
   name                = "terragoat-aks-${var.environment}"
   resource_group_name = azurerm_resource_group.example.name
+  kubernetes_version  = "1.28.9"
+  private_cluster_enabled = true
+  api_server_authorized_ip_ranges = ["10.0.0.0/16"]
+  local_account_disabled = true
+  disk_encryption_set_id = azurerm_disk_encryption_set.example.id
   identity {
     type = "SystemAssigned"
   }
@@ -10,6 +15,13 @@ resource azurerm_kubernetes_cluster "k8s_cluster" {
     name       = "default"
     vm_size    = "Standard_D2_v2"
     node_count = 2
+    vnet_subnet_id = azurerm_subnet.example.id
+    enable_auto_scaling = true
+    min_count = 1
+    max_count = 3
+  }
+  network_profile {
+    network_policy = "azure"
   }
   addon_profile {
     oms_agent {
@@ -17,6 +29,9 @@ resource azurerm_kubernetes_cluster "k8s_cluster" {
     }
     kube_dashboard {
       enabled = false
+    }
+    azure_policy {
+      enabled = true
     }
   }
   role_based_access_control {
